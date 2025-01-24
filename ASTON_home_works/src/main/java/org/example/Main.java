256eaf1afdf75f7979e74c6a3aa3fe573d9a7e82
Kat.Bogdanovich@gmail.com <nik.bogdanovich@gmal.com>
2025-01-25 00:20:29 +0300

    Add task 1

---
+++ ASTON_home_works/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    /*
+   1.Создайте метод printThreeWords(), который при вызове должен отпечатать
+    в столбец три слова: Orange, Banana, Apple.
+   */
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
